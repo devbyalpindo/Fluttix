4dbc1b8092cd4719ae8850dd03d9f131fc157e76
alpindo <aldo.alpindo@yahoo.com>
2021-02-18 23:09:29 +0700

    initial commit

---
+++ android/app/src/main/kotlin/com/flutix/alpindo/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.flutix.alpindo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/shared/shared_value.dart
@@ -0,0 +1,8 @@
+part of 'shared.dart';
+
+String apiKey = "eb055bfe40774eaf34b945314e1189a8";
+String imageBaseURL = "https://image.tmdb.org/t/p/";
+
+PageEvent prevPageEvent;
+
+File imageFileToUpload;
